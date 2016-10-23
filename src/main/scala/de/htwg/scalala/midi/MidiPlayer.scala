c8bb37e6b87b7a6f75bbccc349dd776fef52cc7d
mboger <marko.boger@gmail.com>
2016-10-23 21:46:21 +0200

    Added spec for MidiPlayer

--- src/main/scala/de/htwg/scalala/midi/MidiPlayer.scala
+++ src/main/scala/de/htwg/scalala/midi/MidiPlayer.scala
@@ -4,40 +4,34 @@ package de.htwg.scalala.midi
 import de.htwg.scalala.music.Key
 
 import javax.sound.midi.{ MidiSystem, Synthesizer }
-import javax.sound.sampled.{ AudioInputStream, LineEvent, LineListener, AudioSystem }
-import java.io.File
-import java.net.URL
 
-case class MidiPlayer() {
+class MidiPlayer() {
 
   val synthesizer = MidiSystem.getSynthesizer
-  synthesizer.open()
-  val channels = synthesizer.getChannels
-  val soundBank = synthesizer.getDefaultSoundbank
-  val instruments = soundBank.getInstruments
   synthesizer.open
+  val channels = synthesizer.getChannels
 
-  def play: Unit = play(key = 60, duration = 800, volume = 93)
-  def play(key: Int = 60, duration: Int = 800, volume: Int = 93, channelID: Int = 0): Unit = {
+  def play: Unit = play(key = 60, duration = 800, volume = 75)
+  def play(key: Int = 60, duration: Int = 800, volume: Int = 75, channelID: Int = 0): Unit = {
     val channel = channels.apply(channelID)
     channel.noteOn(key, volume)
     Thread.sleep(duration)
     channel.noteOff(key, volume)
   }
   def play(set: Set[Key]): Unit = {
-    val duration = set.toVector(0).duration
-    val volume = set.toVector(0).volume
+    val duration = set.head.duration
+    val volume = set.head.volume
     set.foreach { key => start(key = key.midiNumber, volume) }
     Thread.sleep(duration)
     set.foreach { key => stop(key.midiNumber, volume) }
   }
 
-  def start(key: Int, volume: Int, channelID: Int = 0): Unit = {
+  def start(key: Int, volume: Int=75, channelID: Int = 0): Unit = {
     val channel = channels.apply(channelID)
     channel.noteOn(key, volume)
   }
 
-  def stop(key: Int, volume: Int, channelID: Int = 0): Unit = {
+  def stop(key: Int, volume: Int=75, channelID: Int = 0): Unit = {
     val channel = channels.apply(channelID)
     channel.noteOff(key, volume)
   }
@@ -45,7 +39,6 @@ case class MidiPlayer() {
   def changeToInstrument(instrumentID: Int = 0, channelID: Int = 0) = {
     val channel = channels(channelID)
     channel.programChange(instrumentID)
-    
   }
 
 }
---
+++ src/test/scala/de/htwg/scalala/midi/MidiPlayerSpec.scala
@@ -0,0 +1,35 @@
+package de.htwg.scalala.midi
+
+import de.htwg.scalala.music.Key
+
+import org.scalatest.WordSpec
+import org.scalatest.Matchers
+import scala.language.postfixOps
+
+class MidiPlayerSpec extends WordSpec {
+
+  "A MidiPlayer" should {
+    val midiplayer = new MidiPlayer
+
+    "play Keys encoded in midiKeys (0-127, c is key 60) for a duration in milliseconds" in {
+      midiplayer.play(key = 60, duration = 100)
+      Thread.sleep(100)
+    }
+    "play a set of Keys simultaneously, needed for chords" in {
+      val set = Set(new Key(60), new Key(64), new Key(67))
+      midiplayer.play(set)
+    }
+    "start and stop a key with some delay inbetween. This is needed to play several keys at once." in {
+      midiplayer.start(60)
+      Thread.sleep(100)
+      midiplayer.stop(60)
+    }
+    "be able to change instrument" in {
+      midiplayer.changeToInstrument(40)
+      midiplayer.play(60, 100)
+      Thread.sleep(200)
+    }
+    
+  }
+
+}
